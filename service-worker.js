2c6738a5e25a603dc968b2c516066f0dbb87f0f6
Abbotlight <shanedanson@gmail.com>
2026-09-10 21:58:47 +0100

    Add offline support

---
+++ service-worker.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = "recipe-book-v1";
+
+const FILES_TO_CACHE = [
+    "./",
+    "./index.html",
+    "./manifest.json",
+    "./icon-512.png",
+    "./images/gummies.jpg",
+    "./images/chicken-pizza.jpg"
+];
+
+self.addEventListener("install", function(event) {
+
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache) {
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+
+});
+
+self.addEventListener("fetch", function(event) {
+
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+
+            if (response) {
+                return response;
+            }
+
+            return fetch(event.request);
+
+        })
+    );
+
+});
